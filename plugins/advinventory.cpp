410f200a735080484af079f3463dd52cd0980919
Carabus <carabusx@gmail.com>
2014-07-24 10:13:26 +0200

    Added advinventory.cpp - snapshot 6

--- plugins/advinventory.cpp
+++ plugins/advinventory.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <set>
+//#include <algorithm>
 
 #include <VTableInterpose.h>
 
@@ -19,6 +20,16 @@
 #include "df/unit.h"
 #include "df/unit_inventory_item.h"
 #include "df/item.h"
+#include "df/item_helmst.h"
+#include "df/item_pantsst.h"
+#include "df/item_armorst.h"
+#include "df/item_glovesst.h"
+#include "df/item_shoesst.h"
+#include "df/itemdef_helmst.h"
+#include "df/itemdef_pantsst.h"
+#include "df/itemdef_armorst.h"
+#include "df/itemdef_glovesst.h"
+#include "df/itemdef_shoesst.h"
 
 using std::string;
 using std::vector;
@@ -66,25 +77,27 @@ void OutputHotkeyString(const int &x, int y, const char *text, const std::string
 
 
 struct SortableItem {
-	union {
-		const df::unit_inventory_item* inv_item;
-		df::item* item;
-	};
-	uint32_t sortingKey;
-	int8_t color;
+	const df::unit_inventory_item *const inv_item;
+	df::item *const item;
 
-	vector< SortableItem* > containedItems;
+	int8_t color;
+	const int8_t indent;
+	bool collapsed;
+
+	uint32_t sortingKey;
+	vector< SortableItem* >* containedItems;
+	vector< SortableItem* >* containedItemsSorted;
 
 
 	SortableItem (const df::unit_inventory_item* _inv_item)
-		: inv_item(_inv_item)//, sortingKey( getSortingKey(_inv_item) )
+		: inv_item(_inv_item), item(_inv_item->item), indent(0), collapsed(false)
 	{
 		sortingKey = 0;
 		calculateKey_Mode_ItemType_BodyPart();
 	}
 
-	SortableItem (df::item* _item)
-		: item(_item)
+	SortableItem (df::item* _item, int8_t _indent)
+		: inv_item(nullptr), item(_item), indent(_indent), collapsed(false)
 	{
 		color = COLOR_LIGHTGREEN;
 		sortingKey = 0;
@@ -116,17 +129,19 @@ struct SortableItem {
 				key = 3;
 		}
 
-		sortingKey |= (key << (4 + 16));
+		sortingKey |= (key << (4 + 16 + 2 + 8)); //30
 
-		calculateKey_ItemType(inv_item->item);
+		calculateKey_ItemType(item);
 
 		key = (uint32_t)( inv_item->body_part_id );
-		sortingKey |= (key << (0));
+		sortingKey |= (key << (2 + 8)); //10
+
+		calculateKey_Layer(item);
 	}
 
 	void calculateKey_ItemType (df::item *item) {
 		uint32_t key = getKey_ItemType(item);
-		sortingKey |= (key << (16));
+		sortingKey |= (key << (16 + 2 + 8)); //26
 	}
 
 	static uint32_t getKey_ItemType (df::item *item)
@@ -152,8 +167,8 @@ struct SortableItem {
 				return 7;
 			case COIN:
 				return 8;
-		//	default:
-		//		return 9;
+			default:
+				return 9; // other items
 			case FLASK:
 				return 10;
 			case QUIVER:
@@ -163,10 +178,119 @@ struct SortableItem {
 			case BOX:
 			case BIN:
 				return 13;  //containers at the end
-			default:
-				return 9;
 		}
 	}
+
+
+	void calculateKey_Layer (df::item *item) {
+		df::armor_properties* props = getArmorProperties(item);
+
+		if (!props)
+			return;
+
+		uint32_t key;
+		key = (uint32_t)( props->layer );
+		sortingKey |= (key << (8)); //8
+
+		key = (uint32_t)( props->layer_permit );
+		key = std::min( key, 255U );
+		sortingKey |= (key << (0));
+	}
+
+	static df::armor_properties* getArmorProperties (df::item *item) {
+		df::armor_properties* props = nullptr;
+
+		using namespace df::enums::item_type;
+
+		switch (item->getType()) {
+			case HELM:
+			{
+				auto armorItem = virtual_cast<df::item_helmst>(item);
+				if (armorItem)
+					props = &armorItem->subtype->props;
+				break;
+			}
+			case ARMOR:
+			{
+				//auto armorItem = (df::item_armorst*)item;
+				auto armorItem = virtual_cast<df::item_armorst>(item);
+				if (armorItem)
+					props = &armorItem->subtype->props;
+				break;
+			}
+			case PANTS:
+			{
+				auto armorItem = virtual_cast<df::item_pantsst>(item);
+				if (armorItem)
+					props = &armorItem->subtype->props;
+				break;
+			}
+			case GLOVES:
+			{
+				auto armorItem = virtual_cast<df::item_glovesst>(item);
+				if (armorItem)
+					props = &armorItem->subtype->props;
+				break;
+			}
+			case SHOES:
+			{
+				auto armorItem = virtual_cast<df::item_shoesst>(item);
+				if (armorItem)
+					props = &armorItem->subtype->props;
+				break;
+			}
+			default:
+				break;
+		}
+
+		return props;
+	}
+};
+
+
+struct Inventory {
+	vector< SortableItem* > items;
+	vector< SortableItem* > itemsSorted;
+	int32_t totalItems;
+
+
+	Inventory()
+		: totalItems(0)
+	{
+
+	}
+};
+
+
+enum InventoryAction {
+    ACTION_DROP,
+    ACTION_EAT,
+    ACTION_GET,
+    ACTION_INTERACT,
+    ACTION_PUT,
+    ACTION_REMOVE,
+    ACTION_THROW,
+    ACTION_VIEW,
+    ACTION_WEAR,
+	NUM_ACTIONS
+};
+
+
+struct InventoryActionInfo {
+	const df::interface_key key;
+	const std::string label;
+};
+
+InventoryActionInfo inventoryActions [NUM_ACTIONS] = {
+	{ interface_key::CUSTOM_SHIFT_D, "rop" },
+	{ interface_key::CUSTOM_SHIFT_E, "at" }, // "at or drink"
+	{ interface_key::CUSTOM_SHIFT_G, "et" },
+	{ interface_key::CUSTOM_SHIFT_I, "nteract" },
+	{ interface_key::CUSTOM_SHIFT_P, "ut" },
+	{ interface_key::CUSTOM_SHIFT_R, "emove" },
+	{ interface_key::CUSTOM_SHIFT_T, "hrow" },
+	{ interface_key::CUSTOM_SHIFT_V, "iew" },
+	{ interface_key::CUSTOM_SHIFT_W, "ear" }
 };
 
 
@@ -174,6 +298,7 @@ class ViewscreenAdvInventory : public dfhack_viewscreen
 {
 public:
 	static bool isActive;
+	static InventoryAction currentAction;
 
 
 	static void show()
@@ -227,7 +352,16 @@ public:
 			}*/
 
             return;
-        }
+        } else {
+			for (int i = 0; i < NUM_ACTIONS; i++) {
+				if (input->count(inventoryActions[i].key)) {
+					if (i != currentAction) {
+						currentAction = (InventoryAction)i;
+					}
+					return;
+				}
+			}
+		}
     }
 
     void render()
@@ -240,14 +374,27 @@ public:
         Screen::clear();
         //Screen::drawBorder("  Advanced Inventory  ");
 
+		int x, y;
 		auto dim = Screen::getWindowSize();
 		OutputString(0, 0, "Advanced Inventory");
 		OutputString(dim.x - 2, 0, "DFHack", COLOR_LIGHTMAGENTA, COLOR_BLACK, true);
 
 		OutputString(dim.x - 2, dim.y - 1, "Written by Carabus/Rafal99", COLOR_DARKGREY, COLOR_BLACK, true);
 
-		int x = OutputHotkeysLine1(dim.y - 1);
+		x = OutputHotkeysLine1(dim.y - 1);
 		OutputHotkeyString(x, dim.y - 1, "Basic Inventory   ", Screen::getKeyDisplay(interface_key::CHANGETAB), COLOR_LIGHTRED); //gps->dimy - 2
+
+		x = 0; y = dim.y - 4;
+		OutputString(x, y, "Actions:", COLOR_WHITE);
+		x += 3;
+
+		for (int i = 0; i < NUM_ACTIONS; i++) {
+			OutputString(x, y, Screen::getKeyDisplay( inventoryActions[i].key ), COLOR_LIGHTGREEN);
+			OutputString(x, y, inventoryActions[i].label, (i == currentAction) ? ((currentAction == ACTION_VIEW) ? COLOR_LIGHTCYAN : COLOR_LIGHTRED) : COLOR_GREY);
+			x += 3;
+		}
+
+		OutputString(79, dim.y - 5, "|");
 	}
 
 	string getFocusString() { return "adv_inventory"; }
@@ -286,6 +433,7 @@ private:
 };
 
 bool ViewscreenAdvInventory::isActive = false;
+InventoryAction ViewscreenAdvInventory::currentAction = ACTION_VIEW;
 int ViewscreenAdvInventory::tabHotkeyStringX = -1;
 
 
@@ -298,11 +446,9 @@ struct advinventory_hook : df::viewscreen_dungeonmodest {
 
     DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key> *input))
     {
-        using namespace ui_advmode_menu;
-
         switch (ui_advmode->menu)
         {
-			case Default:
+			case ui_advmode_menu::Default:
 				if (input->count(interface_key::CUSTOM_CTRL_I))
 				{
 					ViewscreenAdvInventory::isActive = true;
@@ -310,7 +456,7 @@ struct advinventory_hook : df::viewscreen_dungeonmodest {
 					input->insert(interface_key::A_INV_LOOK);
 				}
 				break;
-			case Inventory:
+			case ui_advmode_menu::Inventory:
 				if (input->count(interface_key::CHANGETAB))
 				{
 					ViewscreenAdvInventory::isActive = true;
@@ -345,13 +491,14 @@ struct advinventory_hook : df::viewscreen_dungeonmodest {
 
 		INTERPOSE_NEXT(render)();
 
-		//OutputString(0, 0, enum_item_key(ui_advmode->menu).append("  "));
+		//std::stringstream stream;
+		//stream << "0x" << std::hex << (int)Gui::getCurViewscreen() << " ";
+		//OutputString(0, 1, stream.str());
+		//OutputString(0, 1, enum_item_key(ui_advmode->menu).append("  "));
 
- 		using namespace ui_advmode_menu;
-		
 		switch (ui_advmode->menu)
         {
-			case Inventory:
+			case ui_advmode_menu::Inventory:
 			{
 				int x = ViewscreenAdvInventory::getTabHotkeyStringX();
 				OutputHotkeyString(x, 24, "Advanced Inventory", Screen::getKeyDisplay(interface_key::CHANGETAB), COLOR_LIGHTRED); // gps->dimy - 2
