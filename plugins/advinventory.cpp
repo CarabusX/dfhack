c4691748eab15a41aec0ceeace9dfafadf6e0424
Carabus <carabusx@gmail.com>
2014-07-24 10:04:29 +0200

    Added advinventory.cpp - snapshot #1 (out of 12 versions of its backup copies saved on my disk)

---
+++ plugins/advinventory.cpp
@@ -0,0 +1,71 @@
+#include "Console.h"
+#include "Core.h"
+#include "DataDefs.h"
+#include "Export.h"
+#include "PluginManager.h"
+
+//#include "df/world.h"
+
+using namespace DFHack;
+//using namespace df::enums;
+
+command_result advinventory (color_ostream &out, std::vector <std::string> & parameters);
+
+DFHACK_PLUGIN("advinventory");
+
+DFhackCExport command_result plugin_init ( color_ostream &out, std::vector <PluginCommand> &commands)
+{
+    commands.push_back(PluginCommand(
+        "advinventory",
+        "Replaces adventure mode inventory screens",
+        advinventory,
+        true, //allow non-interactive use
+        "I am a long help string :)"
+    ));
+    return CR_OK;
+}
+
+DFhackCExport command_result plugin_shutdown ( color_ostream &out )
+{
+    return CR_OK;
+}
+
+// Called to notify the plugin about important state changes.
+// Invoked with DF suspended, and always before the matching plugin_onupdate.
+// More event codes may be added in the future.
+/*
+DFhackCExport command_result plugin_onstatechange(color_ostream &out, state_change_event event)
+{
+    switch (event) {
+    case SC_GAME_LOADED:
+        // initialize from the world just loaded
+        break;
+    case SC_GAME_UNLOADED:
+        // cleanup
+        break;
+    default:
+        break;
+    }
+    return CR_OK;
+}
+*/
+
+// Whatever you put here will be done in each game step. Don't abuse it.
+// It's optional, so you can just comment it out like this if you don't need it.
+/*
+DFhackCExport command_result plugin_onupdate ( color_ostream &out )
+{
+    // whetever. You don't need to suspend DF execution here.
+    return CR_OK;
+}
+*/
+
+command_result advinventory (color_ostream &out, std::vector <std::string> & parameters)
+{
+    if (!parameters.empty())
+        return CR_WRONG_USAGE;
+    CoreSuspender suspend;
+    out.print("Hello! I do nothing, remember?\n");
+    return CR_OK;
+}
+
